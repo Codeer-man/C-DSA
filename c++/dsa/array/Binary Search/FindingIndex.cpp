232a44116566a48de70422643bdfc102f717885c
Codeer-man <mdrmoney34@gmail.com>
2024-09-26 23:13:35 +0545

    Binary search and Pointer

---
+++ c++/dsa/array/Binary Search/FindingIndex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec = {-1, 0, 3, 4, 5, 9, 12};
+    int target = 12;
+
+    int st = 0;
+    int end = vec.size() - 1;
+
+    while (st <= end)
+    {
+        int mid = st+ (end - st) / 2;
+        if (vec[mid] < target)
+        {
+            st = mid + 1;
+        }
+        else if (vec[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            cout << mid;
+            break;
+        }
+    }
+    return 0;
+}
